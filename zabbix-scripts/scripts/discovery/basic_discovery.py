15a1d6c5bfd218a036ce1e41c446f93677c38aaf
Jean-Francois Chevrette <jfchevrette@gmail.com>
2017-09-28 11:16:00 -0400

    default to empty list if discovery fails

--- zabbix-scripts/scripts/discovery/basic_discovery.py
+++ zabbix-scripts/scripts/discovery/basic_discovery.py
@@ -6,8 +6,12 @@ import json
 class BasicDiscoverer(AWSClient):
     def get_instances(self, *args):
         "Runs discovery method and packs result into JSON"
-        data = self.discovery(*args)
-        return json.dumps({"data": data})
+
+        try:
+            data = self.discovery(*args)
+            return json.dumps({"data": data})
+        except:
+            return json.dumps({"data": []})
 
     def discovery(self, *args):
         "Method that should be overriden inside inherited classes"
