2240462e056b16a35c413bc562537f4b5baaba56
Diego Sainz <diego.sainz@sperantus.com>
2018-08-28 09:19:53 -0700

    S3 discovery: return only those buckets that belong to the active region

--- zabbix-scripts/scripts/discovery/s3.py
+++ zabbix-scripts/scripts/discovery/s3.py
@@ -4,11 +4,19 @@ from basic_discovery import BasicDiscoverer
 
 class Discoverer(BasicDiscoverer):
     def discovery(self, *args):
+
         response = self.client.list_buckets()
         data = list()
-        for bucket in response["Buckets"]:
+
+        # list_buckets() will return the buckets in all regions. Get only those in the requested region
+        # See https://stackoverflow.com/questions/49814173/boto3-get-only-s3-buckets-of-specific-region
+        region_buckets = \
+            [bucket["Name"] for bucket in self.client.list_buckets()["Buckets"] \
+            if self.client.get_bucket_location(Bucket=bucket['Name'])['LocationConstraint'] == self.region]
+
+        for bucket_name in region_buckets:
             ldd = {
-                "{#BUCKET_NAME}": bucket['Name'],
+                "{#BUCKET_NAME}": bucket_name,
             }
             data.append(ldd)
         return data
