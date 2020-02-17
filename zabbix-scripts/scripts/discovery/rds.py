2ddda4a4c6912b8f0e9e91ffa545fd63b97f1e94
Diego Sainz <diego.sainz@sperantus.com>
2020-02-16 23:02:45 -0800

    Added support for RDS Clusters discovery / fixed item key ELBv2 naming issue

--- zabbix-scripts/scripts/discovery/rds.py
+++ zabbix-scripts/scripts/discovery/rds.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
 from basic_discovery import BasicDiscoverer
 
-
 class Discoverer(BasicDiscoverer):
     def discovery(self, *args):
+    
+        # Clusters (e.g. Aurora)
+        if "cluster" in args:
+            data = list()
+            response = self.client.describe_db_clusters()
+            for cluster in response["DBClusters"]:
+                ldd = {
+                    "{#CLUSTER_ID}": cluster["DBClusterIdentifier"],
+                    "{#CLUSTER_ENDPOINT}": cluster["Endpoint"],
+                    "{#CLUSTER_READER_ENDPOINT}": cluster["ReaderEndpoint"]
+                }
+                data.append(ldd)
+            return data
+
+        # Standalone RDS instances
         response = self.client.describe_db_instances()
-        data = list()
         for instance in response["DBInstances"]:
+            data = list()
             storage_bytes = int(instance["AllocatedStorage"]) * pow(1024, 3)
             ldd = {
                     "{#STORAGE}": storage_bytes,
                     "{#RDS_ID}": instance["DBInstanceIdentifier"]
             }
             data.append(ldd)
+
         return data
